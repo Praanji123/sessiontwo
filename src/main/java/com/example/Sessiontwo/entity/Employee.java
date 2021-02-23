b1736e2b03840ab0fcda69661936b7799d3903ae
Prateek Srivastava <prateeksrivastava@Prateeks-MacBook-Pro.local>
2021-02-23 17:44:07 +0530

    mongodb

--- src/main/java/com/example/Sessiontwo/entity/Employee.java
+++ src/main/java/com/example/Sessiontwo/entity/Employee.java
@@ -4,46 +4,25 @@ package com.example.Sessiontwo.entity;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-@Entity
+
+
+@Document
 @Getter
 @Setter
 public class Employee {
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    @Id
-    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
-    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
-
+    @MongoId
     private Long id;
+
     private String name;
+
     private String departmentName;
 
 
